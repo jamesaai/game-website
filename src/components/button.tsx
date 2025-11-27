2866c22c5548b6c115a3bd4298391500b7817a40
jamesaai <jgwatson29@gmail.com>
2025-11-26 21:58:34 -0500

    Update button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { IconType } from "react-icons";
 
 interface ButtonProps {
+  id?: string;
   containerClass?: string;
   onClick?: () => void;
+  leftIcon?: IconType;
+  rightIcon?: IconType;
   children: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({ containerClass, onClick, children }) => {
+export const Button: React.FC<ButtonProps> = ({
+  id,
+  containerClass,
+  onClick,
+  leftIcon: LeftIcon,
+  rightIcon: RightIcon,
+  children,
+}) => {
   return (
     <button
-      className={containerClass}
+      id={id}
+      className={`flex items-center justify-center gap-2 ${containerClass}`}
       onClick={onClick}
       type="button"
     >
+      {LeftIcon && <LeftIcon />}
       {children}
+      {RightIcon && <RightIcon />}
     </button>
   );
 };
