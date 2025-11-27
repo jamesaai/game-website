6c97b2a8b7e3cfc6b0194bf88b4dfd3b35825fa6
jamesaai <jgwatson29@gmail.com>
2025-11-26 21:19:27 -0500

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,29 @@
-import { About } from "@/components/about";
-import { Contact } from "@/components/contact";
-import { Features } from "@/components/features";
-import { Footer } from "@/components/footer";
-import { Hero } from "@/components/hero";
-import { Navbar } from "@/components/navbar";
-import { Story } from "@/components/story";
+import { About } from “@/components/about”;
+import { Contact } from “@/components/contact”;
+import { Features } from “@/components/features”;
+import { Footer } from “@/components/footer”;
+import { Hero } from “@/components/hero”;
+import { Navbar } from “@/components/navbar”;
+import { Story } from “@/components/story”;
 
 const App = () => {
-  return (
-    <div className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar />
+return (
+<div className="relative min-h-screen w-screen overflow-x-hidden">
+<Navbar />
 
-      <main>
-        <Hero />
-        <About />
-        <Features />
-        <Story />
-        <Contact />
-      </main>
+```
+  <main>
+    <Hero />
+    <About />
+    <Features />
+    <Story />
+    <Contact />
+  </main>
 
-      <Footer />
-    </div>
-  );
+  <Footer />
+</div>
+```
+
+);
 };
 export default App;
